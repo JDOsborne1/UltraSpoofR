b47baed55b1f759610df3c67359727168ad69564
Joe Osborne <joe@osborne.uk.com>
2019-08-03 22:41:28 +0000

    Updated some tests to deal with deprecation warnings

--- tests/testthat/test-aggRandom.R
+++ tests/testthat/test-aggRandom.R
@@ -1,5 +1,5 @@
 test_that("random generation in range", {
   testvect <- sample(1:100, 10)
-  expect_less_than(ultraAggRandom(testvect), max(testvect))
-  expect_more_than(ultraAggRandom(testvect), min(testvect))
+  expect_lt(ultraAggRandom(testvect), max(testvect))
+  expect_gt(ultraAggRandom(testvect), min(testvect))
 })
