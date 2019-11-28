d0ae4ba5849bf0a99e907f7633689ae461ec8532
JDO <atharos@vivaldi.net>
2019-11-28 00:19:49 +0000

    adding Continuous check tests

--- tests/testthat/test-nominalCheck.R
+++ tests/testthat/test-nominalCheck.R
@@ -1,3 +1,9 @@
 test_that("Nominal checking works", {
   expect_equal(ultraNominalCheck(irisUltra), c("Species"))
 })
+test_that("Continuous checking works", {
+  expect_equal(ultraContinuousCheck(irisUltra), c("Sepal.Length"
+                                                  ,"Sepal.Width"
+                                                  ,"Petal.Length"
+                                                  ,"Petal.Width" ))
+})
