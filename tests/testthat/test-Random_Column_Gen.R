064f9e4cb65885a66691da841cb95816ae4b63ad
JDO <atharos@vivaldi.net>
2019-11-30 19:34:38 +0000

    Removing old spoofing functions

--- tests/testthat/test-Random_Column_Gen.R
+++
@@ -1,5 +0,0 @@
-test_that("Random Generation works for Values", {
-  expect_length(ultraRandoColumn(irisUltra, "Sepal.Width")$Sepal.Width, 100)
-  expect_false(anyNA(ultraRandoColumn(irisUltra, "Sepal.Width")$Sepal.Width))
-  expect_lt(as.numeric(max(irisUltra$spoof$Sepal.Width)), max(irisUltra$origin()$Sepal.Width))
-})
